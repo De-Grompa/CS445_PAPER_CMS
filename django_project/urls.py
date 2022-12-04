0aaac7a9154da68ff096b1268897444fdff52940
De-Grompa <jeremywalz99@gmail.com>
2022-12-04 16:36:34 -0600

    hyperlinked myBinder and gradeView
    Added myBinder to all accounts, and locked gradeView to be viewed only when viewed on an admin account. Next I'll setup the grouping system.. Should be: Admin (can see all) > Professor (can see per class that they're assigned) > student (can only see their OWN assignments). Aside from permission table, we also need to setup a class table where professors can only see the student's papers from the class that they are also assigned.

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,8 +20,12 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     #ADMIN
     path('admin/', admin.site.urls),
+    path('account/gradeView', TemplateView.as_view(template_name='gradeView.html'), name='gradeView'),
     #FOR NON_ADMIN
     path('account/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('account/register/', TemplateView.as_view(template_name='register.html'), name='register'),
+    path('account/myBinder', TemplateView.as_view(template_name='myBinder.html'), name = 'myBinder'),
+    path('account/paperView', TemplateView.as_view(template_name='paperView.html'), name='paperView'),
+    path('account/gradeView', TemplateView.as_view(template_name='gradeView.html'), name='gradeView'),
     ]
